e749a0d6a8a923e0f4700c1719bb364952a99258
Jerry <calljerryli@outlook.com>
2022-02-11 16:24:42 +0800

    parse env PORT

--- jfi_http_api/src/main.rs
+++ jfi_http_api/src/main.rs
@@ -19,7 +19,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("listening on {}", addr);
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
